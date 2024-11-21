2502eb2b16267625992644ede5b4723e4de7ea46
Wenting Y <ywtzoe@gmail.com>
2024-11-21 13:04:32 -0500

    Clean css-set-up

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,4 +1,4 @@
 import styles from "./about.module.scss";
 export default function About() {
-  return <p className={styles.test}>About Page</p>;
+  return <p className={styles.test}>About Page </p>;
 }
