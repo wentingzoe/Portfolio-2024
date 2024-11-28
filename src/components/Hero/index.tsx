326298bce7db6f1eec06b23bbc3b7920f5a9a8e3
Wenting Y <ywtzoe@gmail.com>
2024-11-27 21:55:36 -0500

    Refactor Hero component layout and styles, adding name and location sections with responsive design

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -13,20 +13,30 @@ export default function Hero() {
       <BreakpointProvider>
         <FloatingBox />
       </BreakpointProvider>
-      {/* <div className={styles.hero__title}>
-        <Creative />
-        <Titles />
-      </div> */}
-      {/* <div className={styles.hero__bg}>
-        <Image
-          className={styles.hero__image}
-          src="/images/hero_left_bg.svg"
-          alt="bg"
-          fill
-          priority
-        />
-      </div> */}
 
+      <div className={styles.hero__content}>
+        <div className={styles.hero__name}>
+          <h2 className={styles.hero__name__fn}>Wenting</h2>
+          <h2 className={styles.hero__name__ln}>Yong</h2>
+        </div>
+        <div className={styles.hero__title}>
+          <Creative />
+          <Titles />
+          <p className={styles.hero__title__subtitle}>
+            specialized in Web Design, UX / UI, SEO, and Front End Development.
+          </p>
+        </div>
+        <div className={styles.hero__location}>
+          <div className={styles.hero__location__icon}>
+            <Image src="/images/star.svg" alt="location" fill priority />
+          </div>
+          <p className={`${styles.hero__location__time} small`}>
+            Based in Canada EST 08:42:31 PM
+          </p>
+        </div>
+      </div>
+
+      {/* 
       <div className={styles.hero__portrait}>
         <Image
           className={styles.hero__image}
@@ -35,7 +45,7 @@ export default function Hero() {
           fill
           priority
         />
-      </div>
+      </div> */}
     </div>
   );
 }
