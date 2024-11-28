319e07cafe00c55a83ca1e62ccbd5452eeb0b1d1
Wenting Y <ywtzoe@gmail.com>
2024-11-28 11:46:13 -0500

    Add real-time clock to Hero component, updating displayed time every second

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./hero.module.scss";
 import Image from "next/image";
 import Creative from "./Creative";
@@ -7,6 +7,31 @@ import Titles from "./Titles";
 import FloatingBox from "./FloatingBox";
 
 export default function Hero() {
+  const [currentTime, setCurrentTime] = useState<string>("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const options: Intl.DateTimeFormatOptions = {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+      };
+      const formattedTime = now.toLocaleTimeString("en-US", options);
+      setCurrentTime(formattedTime);
+    };
+
+    // Update time initially
+    updateTime();
+
+    // Set interval to update time every second
+    const intervalId = setInterval(updateTime, 1000);
+
+    // Clean up the interval on unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className={styles.hero}>
       <FloatingBox />
@@ -27,7 +52,7 @@ export default function Hero() {
             <Image src="/images/star.svg" alt="location" fill priority />
           </div>
           <p className={`${styles.hero__location__time} small`}>
-            Based in Canada EST 08:42:31 PM
+            Based in Canada EST {currentTime}
           </p>
         </div>
       </div>
