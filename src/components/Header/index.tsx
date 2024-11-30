b857078715545a51a7a06df04635645e840d6b67
Wenting Y <ywtzoe@gmail.com>
2024-11-30 12:41:46 -0500

    Refactor Header component to import menu animation from utils for improved code organization

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,28 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import Button from "./Button";
 import styles from "./header.module.scss";
 import Nav from "./Nav/index";
-
-const menu = {
-  open: {
-    width: "45vw",
-    height: "45vw",
-    top: "3vw",
-    right: "3vw",
-    transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
-  },
-  closed: {
-    width: "5vw",
-    height: "5vw",
-    top: "0",
-    right: "0",
-    transition: {
-      duration: 0.75,
-      delay: 0.35,
-      type: "tween",
-      ease: [0.76, 0, 0.24, 1],
-    },
-  },
-};
+import { menu } from "@/utils/animation";
 
 export default function Header() {
   const [isActive, setIsActive] = useState(false);
