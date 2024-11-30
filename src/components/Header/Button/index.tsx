2a4023771b5e3e458671cf6d8fc0c935c77522a2
Wenting Y <ywtzoe@gmail.com>
2024-11-30 12:41:30 -0500

    Refactor Button component styles and structure for improved layout and visual enhancements

--- src/components/Header/Button/index.tsx
+++ src/components/Header/Button/index.tsx
@@ -8,19 +8,26 @@ interface ButtonProps {
 
 export default function Button({ isActive, toggleMenu }: ButtonProps) {
   return (
-    <div className={styles.button} onClick={toggleMenu}>
-      <motion.div
-        className={styles.slider}
-        animate={{ top: isActive ? "-100%" : "0%" }}
-        transition={{ duration: 0.5, type: "tween", ease: [0.76, 0, 0.24, 1] }}
-      >
-        <div className={styles.el}>
-          <PerspectiveText label="Menu" />
-        </div>
-        <div className={styles.el}>
-          <PerspectiveText label="Close" />
-        </div>
-      </motion.div>
+    <div className={styles.buttonWrapper}>
+      <div className={styles.bg}></div>
+      <div className={styles.button} onClick={toggleMenu}>
+        <motion.div
+          className={styles.slider}
+          animate={{ top: isActive ? "-100%" : "0%" }}
+          transition={{
+            duration: 0.5,
+            type: "tween",
+            ease: [0.76, 0, 0.24, 1],
+          }}
+        >
+          <div className={styles.el}>
+            <PerspectiveText label="Menu" />
+          </div>
+          <div className={styles.el}>
+            <PerspectiveText label="Close" />
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
