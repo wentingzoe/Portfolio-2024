0383f19c5799e6320a67d18759fe3822630d19d9
Wenting Y <ywtzoe@gmail.com>
2024-11-30 13:13:42 -0500

    Update Header styles for improved color scheme and enhance animation delay logic in utils

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -26,18 +26,20 @@ export const perspective = {
         translateY: 80,
         translateX: -20,
     },
-    enter: (custom: number) => ({
+    enter: (custom: number) =>{ 
+        console.log("Custom index:", custom);
+        return{
         opacity: 1,
         rotateX: 0,
         translateY: 0,
         translateX: 0,
         transition: {
             duration: 0.65, 
-            delay: 0.5 + (custom * 0.1), 
+            delay: 0.5 + (custom * 0.15), 
             ease: [.215,.61,.355,1],
             opacity: { duration: 0.35}
         }
-    }),
+    }},
     exit: {
         opacity: 0,
         transition: { duration: 0.5, type: "linear", ease: [0.76, 0, 0.24, 1]}
