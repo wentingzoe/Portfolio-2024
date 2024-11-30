f7334b53791f633022bebe766edebefe22cd66d5
Wenting Y <ywtzoe@gmail.com>
2024-11-30 14:51:08 -0500

    Update SCSS configuration and remove unused mixins for improved styling consistency

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-
+import path from "path";
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"], 
+      includePaths: [path.join(__dirname, "src/app")],
+    prependData: `@use "@/app/mixins" as *;`,
   }
 };
 
