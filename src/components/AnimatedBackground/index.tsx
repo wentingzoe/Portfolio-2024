bc3ab1bcdc21e55c77eb5d5e48ba08694b800d74
Wenting Y <ywtzoe@gmail.com>
2024-11-29 21:04:47 -0500

    Add AnimatedBackground component with dynamic gradient and blob animations

---
+++ src/components/AnimatedBackground/index.tsx
@@ -0,0 +1,9 @@
+// AnimatedBackground.tsx
+import React from "react";
+import styles from "./animatedbackground.module.scss";
+
+const AnimatedBackground: React.FC = () => {
+  return <div className={styles.backgroundWrapper}></div>;
+};
+
+export default AnimatedBackground;
