29bee4e68ce1b49c45550232d25b413de64ec00d
Wenting Y <ywtzoe@gmail.com>
2024-11-30 10:32:42 -0500

    Add Nav component with animated links and footer for header

---
+++ src/components/Header/Nav/index.tsx
@@ -0,0 +1,48 @@
+import styles from "./nav.module.scss";
+import { motion } from "framer-motion";
+import { links, footerLinks } from "./data";
+import { perspective, slideIn } from "./anim";
+
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.body}>
+        {links.map((link, i) => {
+          const { title, href } = link;
+          return (
+            <div key={`b_${i}`} className={styles.linkContainer}>
+              <motion.a
+                href={href}
+                custom={i}
+                variants={perspective}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              >
+                {title}
+              </motion.a>
+            </div>
+          );
+        })}
+      </div>
+      <motion.div className={styles.footer}>
+        {footerLinks.map((link, i) => {
+          const { title, href } = link;
+          return (
+            <motion.a
+              href={href}
+              variants={slideIn}
+              custom={i}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+              key={`f_${i}`}
+            >
+              {title}
+            </motion.a>
+          );
+        })}
+      </motion.div>
+    </nav>
+  );
+}
