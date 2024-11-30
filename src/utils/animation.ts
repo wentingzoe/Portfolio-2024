d8846d7ed92790bb6ac75642f0aa64df9483a36b
Wenting Y <ywtzoe@gmail.com>
2024-11-30 12:41:12 -0500

    Refactor animation file structure and add menu animation states for improved navigation

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,3 +1,24 @@
+export const menu = {
+  open: {
+    width: "45vw",
+    height: "45vw",
+    top: "-2vw",
+    right: "-2vw",
+    transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
+  },
+  closed: {
+    width: "5vw",
+    height: "5vw",
+    top: "0",
+    right: "0",
+    transition: {
+      duration: 0.75,
+      delay: 0.35,
+      type: "tween",
+      ease: [0.76, 0, 0.24, 1],
+    },
+  },
+};
 export const perspective = {
     initial: {
         opacity: 0,
@@ -5,14 +26,14 @@ export const perspective = {
         translateY: 80,
         translateX: -20,
     },
-    enter: (i: number) => ({
+    enter: (custom: number) => ({
         opacity: 1,
         rotateX: 0,
         translateY: 0,
         translateX: 0,
         transition: {
             duration: 0.65, 
-            delay: 0.5 + (i * 0.1), 
+            delay: 0.5 + (custom * 0.1), 
             ease: [.215,.61,.355,1],
             opacity: { duration: 0.35}
         }
@@ -42,3 +63,4 @@ export const slideIn = {
         transition: { duration: 0.5, type: "tween", ease: "easeInOut"}
     }
 }
+
