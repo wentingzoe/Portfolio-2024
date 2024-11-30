bb34275104092645252bb0e69299662f350e136d
Wenting Y <ywtzoe@gmail.com>
2024-11-30 10:32:10 -0500

    Add Button component with animated toggle functionality and styles

---
+++ src/components/Header/Button/index.tsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+import styles from "./button.module.scss";
+
+interface ButtonProps {
+  isActive: boolean;
+  toggleMenu: () => void;
+}
+
+export default function Button({ isActive, toggleMenu }: ButtonProps) {
+  return (
+    <div className={styles.button} onClick={toggleMenu}>
+      <motion.div
+        className={styles.slider}
+        animate={{ top: isActive ? "-100%" : "0%" }}
+        transition={{ duration: 0.5, type: "tween", ease: [0.76, 0, 0.24, 1] }}
+      >
+        <div className={styles.el}>
+          <PerspectiveText label="Menu" />
+        </div>
+        <div className={styles.el}>
+          <PerspectiveText label="Close" />
+        </div>
+      </motion.div>
+    </div>
+  );
+}
+
+interface PerspectiveTextProps {
+  label: string;
+}
+
+function PerspectiveText({ label }: PerspectiveTextProps) {
+  return (
+    <div className={styles.perspectiveText}>
+      <span>{label}</span>
+      <span>{label}</span>
+    </div>
+  );
+}
