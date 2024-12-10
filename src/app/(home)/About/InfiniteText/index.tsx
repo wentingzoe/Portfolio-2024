5dd584c5a6788104019cfd39c17460d4485797aa
Wenting Y <ywtzoe@gmail.com>
2024-12-10 11:25:44 -0500

    Fixed the animation direction change by scrolling back.

--- src/app/(home)/About/InfiniteText/index.tsx
+++ src/app/(home)/About/InfiniteText/index.tsx
@@ -3,17 +3,25 @@ import React, { useRef, useEffect } from "react";
 import styles from "./infiniteText.module.scss";
 import gsap from "gsap";
 
-export default function Index() {
+export default function InfiniteText() {
   const firstText = useRef(null);
   const secondText = useRef(null);
-  const slider = useRef(null);
   const infiniteText = useRef(null);
 
   let xPercent = 0;
   const direction = useRef(-1);
 
   useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      direction.current = e.deltaY > 0 ? -1 : 1;
+    };
+
+    window.addEventListener("wheel", handleWheel, { passive: true });
     requestAnimationFrame(animate);
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
   }, []);
 
   const animate = () => {
@@ -24,13 +32,14 @@ export default function Index() {
     }
     gsap.set(firstText.current, { xPercent: xPercent });
     gsap.set(secondText.current, { xPercent: xPercent });
-    requestAnimationFrame(animate);
     xPercent += 0.1 * direction.current;
+
+    requestAnimationFrame(animate);
   };
 
   return (
     <div ref={infiniteText} className={styles.infiniteText}>
-      <div ref={slider} className={styles.infiniteText__slider}>
+      <div className={styles.infiniteText__slider}>
         <h1 ref={firstText}>Wenting Yong</h1>
         <h1 ref={secondText}>Wenting Yong</h1>
       </div>
