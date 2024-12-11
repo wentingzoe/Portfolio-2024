15cee8d0b1d293cdf07975332a37683ac6a87c69
Wenting Y <ywtzoe@gmail.com>
2024-12-11 11:05:20 -0500

    Refactor About component; enhance animation visibility and improve title styling for better layout and user experience

--- src/app/(home)/About/index.tsx
+++ src/app/(home)/About/index.tsx
@@ -1,29 +1,42 @@
 import React, { useRef } from "react";
 import styles from "./about.module.scss";
+import { useInView, motion } from "framer-motion";
 import FollowEye from "@/components/FollowEye";
-import { useMousePosition } from "@/context/MousePositionContext";
 import DotSquare from "@/components/DotSquare";
+import StarLine from "@/components/StarLine";
+import TypingText from "@/components/TypingText";
 import InfiniteText from "@/app/(home)/About/InfiniteText";
 import MoreInfo from "@/app/(home)/About/MoreInfo";
-import StarLine from "@/components/StarLine";
-import { useInView, motion } from "framer-motion";
+import { useMousePosition } from "@/context/MousePositionContext";
 import {
   perspectiveRight,
-  opacity,
+  slideUp,
   containerVariants,
+  opacity,
 } from "@/utils/animation";
 import { expertiseItems } from "@/utils/nav-items";
-import TypingText from "@/components/TypingText";
 import { aboutDescription } from "@/utils/text";
 
 export default function About() {
   const mousePosition = useMousePosition();
   const aboutRef = useRef(null);
-  const isInView = useInView(aboutRef, { amount: 0.5 });
+  const detailsRef = useRef(null);
+  const isInAbout = useInView(aboutRef, { amount: 0.5 });
+  const isInDetails = useInView(detailsRef, { amount: 0.6 });
   return (
-    <motion.div ref={aboutRef} className={styles.about}>
+    <motion.div
+      ref={aboutRef}
+      variants={containerVariants}
+      initial="hidden"
+      animate={isInAbout ? "visible" : "hidden"}
+      className={styles.about}
+    >
       <div className={styles.about__content}>
-        <h5 className={styles.about__title}>About Me</h5>
+        <h5
+          className={`${styles["about__title"]} ${styles["about__title--dark"]}`}
+        >
+          About Me
+        </h5>
         <div className={styles.about__eyeDecor}>
           <div className={styles.about__followEye}>
             <FollowEye
@@ -35,46 +48,45 @@ export default function About() {
             <DotSquare color="var(--color-light)" />
           </div>
         </div>
-        <motion.div
-          variants={containerVariants}
-          initial="hidden"
-          animate={isInView ? "visible" : "hidden"}
-          className={styles.about__description}
-        >
-          <motion.div variants={opacity} className={styles.about__decor} />
-          <motion.div variants={opacity} className={styles.about__intro}>
+        <motion.div className={styles.about__description}>
+          <motion.div variants={slideUp} className={styles.about__decor} />
+          <motion.div variants={slideUp} className={styles.about__intro}>
             <TypingText
               text={aboutDescription.text1}
               as="h3"
-              className={styles.about__text1}
               speed={0.04}
               initialDelay={1}
-              isVisible={isInView}
+              isVisible={isInAbout}
             />
             <TypingText
               text={aboutDescription.text2}
               as="h4"
-              className={styles.about__text2}
               speed={0.05}
               initialDelay={1.2}
-              isVisible={isInView}
+              isVisible={isInAbout}
             />
           </motion.div>
         </motion.div>
         <div className={styles.about__moreInfo}>
           <MoreInfo />
         </div>
-        <motion.div className={styles.about__infiniteText} variants={opacity}>
+        <motion.div variants={opacity} className={styles.about__infiniteText}>
           <InfiniteText />
         </motion.div>
       </div>
-      <div className={styles.about__details}>
-        <h5 className={styles.about__detailsTitle}>What I do</h5>
-        <p className={styles.about__detailsCTA}>
-          I can assist you in designing a digital product or enhancing an
-          existing one to make it more visually appealing, interactive, and
-          user-friendly.{" "}
-        </p>
+      <div ref={detailsRef} className={styles.about__details}>
+        <h5
+          className={`${styles["about__title"]} ${styles["about__title--left"]}`}
+        >
+          What I do
+        </h5>
+        <TypingText
+          text={aboutDescription.text3}
+          as="p"
+          className={styles.about__detailsCTA}
+          isVisible={isInDetails}
+        />
+
         <div className={styles.about__list}>
           <div className={styles.about__listTitle}>
             <div className={styles.about__listIcon}>
