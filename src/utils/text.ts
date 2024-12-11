3b20e7d37fb8f0278031d70c1cd78b023afb9055
Wenting Y <ywtzoe@gmail.com>
2024-12-10 20:15:44 -0500

    Add aboutDescription object for personalized introduction text

---
+++ src/utils/text.ts
@@ -0,0 +1,16 @@
+export const aboutDescription = {
+  text1: "Hey,\nI’m Wenting (Zoe)!",
+  text2: [
+    { text: "A " },
+    { text: "web developer", className: "bold" },
+    { text: " and " },
+    { text: "digital designer", className: "bold" },
+    { text: ", I love crafting " },
+    { text: "visual", className: "bold" },
+    { text: ", " },
+    { text: "interactive", className: "bold" },
+    { text: ", and " },
+    { text: "responsive", className: "bold" },
+    { text: " web experiences." }
+  ]
+};
