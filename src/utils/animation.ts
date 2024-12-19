516f3f64cbf090c10d803f6283b3e1b6677a7e35
Wenting Y <ywtzoe@gmail.com>
2024-12-19 11:34:49 -0500

    Refactor animation utility; remove unused perspectiveRight variant, enhance slide animations with improved transitions and add new slideRight variant for better visual effects

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,5 +1,3 @@
-import { delay } from "framer-motion";
-import { s } from "framer-motion/client";
 
 export const menu = {
   open: {
@@ -69,32 +67,8 @@ export const slideIn = {
     }
 }
 
-export const perspectiveRight = {
-    initial: {
-        opacity: 0,
-        rotateX: 90,
-        translateX: 80,
-        translateY: -20,
-    },
-    enter: (custom: number) =>{ 
-    
-        return{
-        opacity: 1,
-        rotateX: 0,
-        translateY: 0,
-        translateX: 0,
-        transition: {
-            duration: 1, 
-            delay: 0.5 + (custom * 0.15), 
-            ease: [.215,.61,.355,1],
-            opacity: { duration: 0.35}
-        }
-    }},
-    exit: {
-        opacity: 0,
-        transition: { duration: 0.5, type: "linear", ease: [0.76, 0, 0.24, 1]}
-    }
-}
+
+
 
 //Delay Children Animation
 export const containerVariants = {
@@ -110,11 +84,24 @@ export const containerVariants = {
 
 //Box Animation
 export const slideUp = {
-  hidden: { opacity: 0, scale: 0.5},
+  hidden: { opacity: 0, scale: 0.5, x:-20, y:-20},
   visible: {
     opacity: 1,
     scale: 1,
-    transition: { duration: 0.5,type: "tween", ease: [0.76, 0, 0.24, 1]}
+    x: 0,
+    y: 0,
+    transition: { duration: 1,type: "linear", ease: [0.76, 0, 0.24, 1]}
+  }
+};
+
+export const slideRight = {
+  hidden: { opacity: 0, scale: 0.5, x:-100, y:-20},
+  visible: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+    y: 0,
+    transition: { duration: 1,type: "linear", ease: [0.76, 0, 0.24, 1]}
   }
 };
 
@@ -126,3 +113,24 @@ export const opacity = {
     }
 }
 
+export const perspectiveRight = {
+    hidden: {
+        opacity: 0,
+        transform: "rotateX(-90deg) translateX(-100px) translateY(-50px)",
+         transition: { duration: 1, type: "linear", ease: [0.76, 0, 0.24, 1]}
+    },
+    visible: (custom: number) =>{ 
+    
+        return{
+        opacity: 1,
+        transform: "rotateY(0deg) translateX(0) translateY(0)",
+        transition: {
+            duration: 2, 
+            delay: 2 + (custom * 0.3), 
+            ease: [.215,.61,.355,1],
+            opacity: { duration: 0.35}
+        }
+    }},
+    
+    
+}
