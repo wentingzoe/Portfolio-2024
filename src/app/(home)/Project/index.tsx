c50fb561b9cc9a5d1c5702adaf6fc28fa7162f9e
Wenting Y <ywtzoe@gmail.com>
2025-02-11 15:58:25 -0500

    Add Project component with basic structure and placeholder content

---
+++ src/app/(home)/Project/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+//import styles from "./project.module.scss";
+
+export default function Project() {
+  return (
+    <div>
+      <h1>Project</h1>
+      <p>This is the Project component.</p>
+    </div>
+  );
+}
