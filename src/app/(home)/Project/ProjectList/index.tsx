1fcf6d1ee4309da298277ef59a17d5afda10dcec
Wenting Y <ywtzoe@gmail.com>
2025-02-12 09:17:23 -0500

    Add Lenis for smooth scrolling and enhance ProjectList component with dynamic scaling

--- src/app/(home)/Project/ProjectList/index.tsx
+++ src/app/(home)/Project/ProjectList/index.tsx
@@ -1,10 +1,44 @@
-import React from "react";
-//import styles from "./projectList.module.scss";
+"use Client";
+import { useEffect, useRef } from "react";
+import { useScroll } from "framer-motion";
+import ProjectCard from "../ProjectCard";
+import { projects } from "@/utils/text";
+import styles from "./projectList.module.scss";
+import Lenis from "lenis";
 
 export default function ProjectList() {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  });
+
   return (
-    <div>
-      <h1>Project List</h1>
+    <div ref={container} className={styles.projectList}>
+      {projects.map((project, i) => {
+        const targetScale = 1 - (projects.length - i) * 0.05;
+        return (
+          <ProjectCard
+            key={`p_${i}`}
+            i={i}
+            {...project}
+            url={project.link}
+            progress={scrollYProgress.get()}
+            range={[i * 0.25, 1]}
+            targetScale={targetScale}
+          />
+        );
+      })}
     </div>
   );
 }
