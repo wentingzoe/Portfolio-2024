eeda37762d1a43d03045c500d6ad21f17406efae
Wenting Y <ywtzoe@gmail.com>
2025-02-12 09:15:59 -0500

    Add project data with titles, descriptions, and images to text utility for Testing

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -15,3 +15,41 @@ export const aboutDescription = {
   ],
 	text3:" I can assist you in designing a digital product or enhancing an existing one to make it more visually appealing, interactive, and user-friendly.",
 };
+
+export const projects = [
+  {
+    title: "Matthias Leidinger",
+    description: "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
+    src: "rock.jpg",
+    link: "",
+    color: "#BBACAF"
+  },
+  {
+    title: "Clément Chapillon",
+    description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
+    src: "tree.jpg",
+    link: "",
+    color: "#977F6D"
+  },
+  {
+    title: "Zissou",
+    description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
+    src: "water.jpg",
+    link: "",
+    color: "#C2491D"
+  },
+  {
+    title: "Mathias Svold and Ulrik Hasemann",
+    description: "The coastlines of Denmark are documented in tonal colors in a pensive new series by Danish photographers Ulrik Hasemann and Mathias Svold; an ongoing project investigating how humans interact with and disrupt the Danish coast.",
+    src: "house.jpg",
+    link: "",
+    color: "#B62429"
+  },
+  {
+    title: "Mark Rammers",
+    description: "Dutch photographer Mark Rammers has shared with IGNANT the first chapter of his latest photographic project, ‘all over again’—captured while in residency at Hektor, an old farm in Los Valles, Lanzarote. Titled ‘Beginnings’, the mesmerizing collection of images is a visual and meditative journey into the origins of regrets and the uncertainty of stepping into new unknowns.",
+    src: "cactus.jpg",
+    link: "",
+    color: "#88A28D"
+  }
+]
