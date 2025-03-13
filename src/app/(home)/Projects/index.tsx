2f14e281b5bf507b16e6f4499106a590b2ce68e4
Wenting Y <ywtzoe@gmail.com>
2025-03-13 11:33:57 -0400

    Refactor Projects component to remove unused scroll handling and improve layout with grid display for titles

--- src/app/(home)/Projects/index.tsx
+++ src/app/(home)/Projects/index.tsx
@@ -3,17 +3,12 @@ import { useState, useRef } from "react";
 import Project from "./Project";
 import Modal from "./Modal";
 import { project_list } from "@/utils/text";
-import { useScroll, useTransform, motion } from "framer-motion";
 
 export default function Index() {
   const [modal, setModal] = useState({ active: false, index: 0 });
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
   const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["start end", "end start"],
-  });
-  const height = useTransform(scrollYProgress, [0, 0.9], [100, 0]);
+
   return (
     <div ref={container} className={styles.projects}>
       <h5 className={styles.projects__title}> Selected Works</h5>
@@ -33,6 +28,7 @@ export default function Index() {
                 details={project.details}
                 src={project.src}
                 tags={project.tags}
+                year={project.year}
                 setModal={setModal}
                 activeIndex={activeIndex}
                 setActiveIndex={setActiveIndex}
@@ -42,12 +38,6 @@ export default function Index() {
         })}
       </div>
       <Modal modal={modal} projects={project_list} />
-      <motion.div
-        style={{ height }}
-        className={styles["projects__circle-container"]}
-      >
-        <div className={styles.projects__circle}></div>
-      </motion.div>
     </div>
   );
 }
