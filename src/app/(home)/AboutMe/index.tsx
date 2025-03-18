cda896a3d302bbb200d65282e7ba18da0428c4c5
Wenting Y <ywtzoe@gmail.com>
2025-03-18 09:58:37 -0400

    Refactor AboutMe component layout: update styles for mobile responsiveness, add borders, and restructure content for improved organization

--- src/app/(home)/AboutMe/index.tsx
+++ src/app/(home)/AboutMe/index.tsx
@@ -17,53 +17,58 @@ export default function AboutMe() {
       {/* Who I am */}
       <div className={styles.aboutMe__who}>
         <h5 className={styles.aboutMe__title}>Who I am</h5>
-        <div className={styles.aboutMe__hello}>
-          <h2>H</h2>
-          <div className={styles.aboutMe__followEye}>
-            <FollowEye
-              mousePosition={mousePosition}
-              backgroundColor="var(--color-primary)"
-            />
-          </div>
-          <h2>llo</h2>
-        </div>
         <div className={styles.aboutMe__content}>
-          <div className={styles.aboutMe__im}>
-            <Image
-              className={styles.aboutMe__image}
-              src="/images/i'm.svg"
-              alt="im"
-              fill
-            />
+          <div className={styles.aboutMe__hello}>
+            <h2>H</h2>
+            <div className={styles.aboutMe__followEye}>
+              <FollowEye
+                mousePosition={mousePosition}
+                backgroundColor="var(--color-primary)"
+              />
+            </div>
+            <h2>llo</h2>
           </div>
-          <div className={styles.aboutMe__name}>
-            <TextSwitch items={about_name} size="h2" />
+          <div className={styles.aboutMe__row}>
+            <div className={styles.aboutMe__im}>
+              <Image
+                className={styles.aboutMe__image}
+                src="/images/i'm.svg"
+                alt="im"
+                fill
+              />
+            </div>
+            <div className={styles.aboutMe__name}>
+              <TextSwitch items={about_name} size="h2" />
+            </div>
           </div>
-          <div className={styles.aboutMe__description}>
-            <p>
-              A web developer & digital designer passionate about crafting bold,
-              interactive, and meaningful experiences.
+          <div className={styles.aboutMe__row}>
+            <div className={styles.aboutMe__decor}>
+              <DotSquare color="var(--color-light)" />
+            </div>
+            <div className={styles.aboutMe__description}>
+              <p>
+                A web developer & digital designer passionate about crafting
+                bold, interactive, and meaningful experiences.
+              </p>
+              <p>
+                With a love for visual storytelling and motion design, I build
+                experiences that captivate and engage users.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.aboutMe__cta}>
+            <div className={styles.aboutMe__starLine}>
+              <StarLine
+                color="var(--color-primary)"
+                lineWidth={200}
+                isInDetails={true}
+              />
+            </div>
+            <p className={`${styles.aboutMe__ctaText} regular`}>
+              Let’s turn ideas into digital reality
             </p>
-            <p>
-              With a love for visual storytelling and motion design, I build
-              experiences that captivate and engage users.
-            </p>
           </div>
-          <div className={styles.aboutMe__decor}>
-            <DotSquare color="var(--color-light)" />
-          </div>
-        </div>
-        <div className={styles.aboutMe__cta}>
-          <div className={styles.aboutMe__starLine}>
-            <StarLine
-              color="var(--color-primary)"
-              lineWidth={200}
-              isInDetails={true}
-            />
-          </div>
-          <p className={`${styles.aboutMe__ctaText} regular`}>
-            Let’s turn ideas into digital reality
-          </p>
         </div>
       </div>
 
