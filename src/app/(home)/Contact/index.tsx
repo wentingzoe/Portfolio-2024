145ad6b3696ba764db3d67013137b28e2676f406
Wenting Y <ywtzoe@gmail.com>
2025-03-26 15:15:23 -0400

    Enhance Contact component layout and styling with new structure and visual elements

--- src/app/(home)/Contact/index.tsx
+++ src/app/(home)/Contact/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./contact.module.scss";
-import { useBreakpoint } from "@/context/BreakpointContext";
+import Image from "next/image";
 import FloatingRect from "@/components/FloatingRect";
+import FollowEye from "@/components/FollowEye";
+import Star from "@/components/Star";
+import { useBreakpoint } from "@/context/BreakpointContext";
+import { useMousePosition } from "@/context/MousePositionContext";
 import { contactFloatingRectConfig } from "@/utils/floatingRectConfigs";
+import { socialLinks } from "@/utils/nav-items";
+import StarLine from "@/components/StarLine";
 
 export default function Contact() {
   const breakpoint = useBreakpoint();
   const decorConfig = contactFloatingRectConfig[breakpoint];
+  const mousePosition = useMousePosition();
   return (
     <div className={styles.contact}>
       <FloatingRect
@@ -15,7 +22,62 @@ export default function Contact() {
       />
 
       <div className={styles.contact__cta}>
-        <h1>Let&apos;s make magic</h1>
+        <div className={styles.contact__content}>
+          <h2>Let&apos;s make</h2>
+          <div className={styles.contact__row}>
+            <h2>magic</h2>
+            <div className={styles.contact__followEye}>
+              <FollowEye
+                mousePosition={mousePosition}
+                backgroundColor="var(--color-secondary)"
+              />
+            </div>
+          </div>
+          <h2>together</h2>
+          <div className={styles.contact__ctaText}>
+            <div className={styles.contact__starLine}>
+              <StarLine
+                color="var(--color-secondary)"
+                lineWidth={400}
+                isInDetails={true}
+              />
+            </div>
+            <h4>Get in Touch</h4>
+          </div>
+        </div>
+      </div>
+      <div className={styles.contact__visual}>
+        <div className={styles.contact__portrait}>
+          <Image
+            className={styles.contact__image}
+            src="/images/contact.webp"
+            alt="portrait"
+            fill
+            priority
+          />
+        </div>
+      </div>
+      <div className={styles.contact__footer}>
+        <div className={styles.contact__copyRight}>
+          <div className={styles.contact__star}>
+            <Star color="var(--color-secondary)" />
+          </div>
+          <p className={`${styles.contact__text} small`}>
+            ©2024 Wenting Yong
+            <br />
+            All Rights Reserved
+          </p>
+        </div>
+        <div className={styles.contact__social}>
+          {socialLinks.map((link, i) => {
+            const { title, href } = link;
+            return (
+              <a href={href} key={`footer_${i}`}>
+                {title}
+              </a>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
