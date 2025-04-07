1253b3ce88d17affec09382718cde2e75bae9e56
Wenting Y <ywtzoe@gmail.com>
2025-04-07 12:22:49 -0400

    Refactor Projects component: Rename styles, remove unused Modal and Project components, and integrate ProjectsList for improved structure

--- src/app/(home)/Projects/index.tsx
+++ src/app/(home)/Projects/index.tsx
@@ -1,43 +1,11 @@
 import styles from "./projects.module.scss";
-import { useState, useRef } from "react";
-import Project from "./Project";
-import Modal from "./Modal";
-import { project_list } from "@/utils/text";
+import ProjectsList from "@/components/ProjectsList";
 
 export default function Index() {
-  const [modal, setModal] = useState({ active: false, index: 0 });
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const projectsRef = useRef(null);
-
   return (
-    <div ref={projectsRef} className={styles.projects}>
+    <div className={styles.projects}>
       <h5 className={styles.projects__title}> Selected Works</h5>
-      <div className={styles.projects__container}>
-        <div className={styles.projects__listTitles}>
-          <h6>Project Name</h6>
-          <h6>Role</h6>
-          <h6>Year</h6>
-        </div>
-        {project_list.map((project, index) => {
-          return (
-            <ul className={styles.projects__line} key={index}>
-              <Project
-                index={index}
-                name={project.name}
-                role={project.role}
-                details={project.details}
-                src={project.src}
-                tags={project.tags}
-                year={project.year}
-                setModal={setModal}
-                activeIndex={activeIndex}
-                setActiveIndex={setActiveIndex}
-              />
-            </ul>
-          );
-        })}
-      </div>
-      <Modal modal={modal} projects={project_list} projectsRef={projectsRef} />
+      <ProjectsList />
     </div>
   );
 }
