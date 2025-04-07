3965a32a494efe2853e462bc9bc6b227dfe9e3a5
Wenting Y <ywtzoe@gmail.com>
2025-04-07 16:41:16 -0400

    Refactor Projects component: Update structure to use useRef for container reference and enhance styles for better layout and responsiveness

--- src/app/(home)/Projects/index.tsx
+++ src/app/(home)/Projects/index.tsx
@@ -1,11 +1,14 @@
-import styles from "./projects.module.scss";
+import { useRef } from "react";
 import ProjectsList from "@/components/ProjectsList";
+import styles from "./projects.module.scss";
 
-export default function Index() {
+export default function Projects() {
+  const homeContainerRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className={styles.projects}>
+    <div ref={homeContainerRef} className={styles.projects}>
       <h5 className={styles.projects__title}> Selected Works</h5>
-      <ProjectsList />
+      <ProjectsList filter="ALL" containerRef={homeContainerRef} />
     </div>
   );
 }
