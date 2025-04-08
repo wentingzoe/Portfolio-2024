bf4307e3e7eaced8e983add9b0ef38b4b5d7e97a
Wenting Y <ywtzoe@gmail.com>
2025-04-07 22:19:09 -0400

    Refactor Projects component: Wrap ProjectsList in a container div and enhance styling with flexbox for improved layout and responsiveness

--- src/app/(home)/Projects/index.tsx
+++ src/app/(home)/Projects/index.tsx
@@ -8,7 +8,9 @@ export default function Projects() {
   return (
     <div ref={homeContainerRef} className={styles.projects}>
       <h5 className={styles.projects__title}> Selected Works</h5>
-      <ProjectsList filter="ALL" containerRef={homeContainerRef} />
+      <div className={styles.projects__container}>
+        <ProjectsList filter="ALL" containerRef={homeContainerRef} />
+      </div>
     </div>
   );
 }
