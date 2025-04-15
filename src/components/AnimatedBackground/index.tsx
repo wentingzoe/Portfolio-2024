3d85b01235053e00d81ce8a8d2f227fa960cd0c9
Wenting Y <ywtzoe@gmail.com>
2025-04-15 13:38:39 -0400

    Replace AnimatedBackground component with GradientBg component for enhanced visual effects and styling

--- src/components/AnimatedBackground/index.tsx
+++
@@ -1,9 +0,0 @@
-// AnimatedBackground.tsx
-import React from "react";
-import styles from "./animatedbackground.module.scss";
-
-const AnimatedBackground: React.FC = () => {
-  return <div className={styles.backgroundWrapper}></div>;
-};
-
-export default AnimatedBackground;
